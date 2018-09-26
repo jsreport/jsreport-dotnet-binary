cc4b182df4f01d9416821721cff20e375e18b9ee
pofider <jan.blaha@hotmail.com>
2018-09-26 09:49:14 +0200

    download binary during build

---
+++ jsreport.Binary/download.ps1
@@ -0,0 +1,2 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(new-object System.Net.WebClient).DownloadFile('https://github.com/jsreport/jsreport/releases/download/2.2.0/jsreport-win.zip','jsreport.zip')
